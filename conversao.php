a2952715ede55e4984b498f0220dd3f603344b59
EribaldoOliveira <desenvolvedorwebst@gmail.com>
2023-11-29 20:14:46 -0200

    Corrigindo conversao.php

--- conversao.php
+++ conversao.php
@@ -10,24 +10,25 @@
     <main>
         <h1>Conversor de Moedas</h1>
         <?php
-        //Cotação copiada do Google
-        $cotacao = 4.87; // em 28/11/23
+            //Cotação copiada do Google
+            $cotacao = 4.87; // em 28/11/23
 
-        //Quantos $$ você tem?
-        $real = $_REQUEST['dinheiro'] ?? 0;
+            //Quantos $$ você tem?
+            $real = $_REQUEST['dinheiro'] ?? 0;
 
-        //Equivalência em dólar
-        $dolar = $real / $cotacao;
+            //Equivalência em dólar
+            $dolar = $real / $cotacao;
 
-        // Mostrar o resultado
-        // echo "Seus R/$" . number_format($real, 2, ',' , '.') . " equivalem a US/$ "  . number_format($dolar, 2, ',' , '.')  
+            // Mostrar o resultado
+            // echo "Seus R/$" . number_format($real, 2, ',' , '.') . " equivalem a US/$ "  . number_format($dolar, 2, ',' , '.')  
 
-        // Formatação de moedas com internacionalização!
-        // Biblioteca inti (Internacionalization PHP)
+            // Formatação de moedas com internacionalização!
+            // Biblioteca inti (Internacionalization PHP)
 
-        $padrão = numfmt_create('pt_BR', NumberFormatter::CURRENCY);
+            $padrao = numfmt_create('pt_BR', NumberFormatter::CURRENCY);
 
-        echo "Seus " . numfmt_format_currency($padrão, $real, 'BRL') . " equivalem a ".numfmt_format_currency($padrão, $dolar, 'USO');
+            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . 
+            " equivalem a ".numfmt_format_currency($padrao, $dolar, "USD");
         ?>
         <button onclick = 'javascript:history.go(-1)'>Voltar</button>
     </main>
